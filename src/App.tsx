f77268466634d9da1ae197ad02e0ec6314e827f2
CosmoumaDD <cosmo4267@gmail.com>
2026-02-04 22:50:00 +0800

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
+import React from 'react'
+
 function App() {
-  
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">
+    <div className="min-h-screen flex flex-col items-center justify-center bg-slate-950 text-slate-100">
+      <h1 className="mb-4 text-3xl font-bold underline">
         This is Cosmo!
       </h1>
-      <p className="text-lg">The Website is under construction...</p>
-      <p className="text-lg">This personal website is built with React, TypeScript and Tailwind CSS.</p>
-      <p className="text-lg">I will update it continuously...</p>
-  </div> 
+      <p className="mb-1 text-lg">
+        The Website is under construction...
+      </p>
+      <p className="mb-1 text-lg">
+        This personal website is built with React, TypeScript and Tailwind CSS.
+      </p>
+      <p className="text-lg">
+        I will update it continuously...
+      </p>
+    </div>
   )
 }
 
